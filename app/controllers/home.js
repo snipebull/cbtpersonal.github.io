69e042172cfe07657d76a846d46a8fbe2438664e
snipebull <fireblond3@gmail.com>
2021-07-30 20:01:21 +0400

    test 123 by ed3ath not me

---
+++ app/controllers/home.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/', (req, res, next) =>
+  /* if (req.get('host') !== 'cbtracker.cwsdev.net' && process.env.NODE_ENV === 'production') {
+    return res.redirect('https://cbtracker.cwsdev.net');
+  } */
+  res.render('index', { title: 'CryptoBlades Tracker' }));
+
+
+router.get('/version', (req, res, next) => res.json({ version: '2.0.21' }));
+
+module.exports = router;
